a27b0ae62028f1a1a72fc14145eb508175d40112
Dominik Marczuk <mingos.nospam@gmail.com>
2009-07-18 00:09:24 +0400

    [Empty Commit Message]

--- src/fov_restrictive.c
+++ src/fov_restrictive.c
@@ -1,6 +1,6 @@
 /*
 * libtcod 1.4.2
-* Copyright (c) 2008,2009 Jice
+* Copyright (c) 2008,2009 J.C.Wilk
 * All rights reserved.
 *
 * Redistribution and use in source and binary forms, with or without
@@ -10,13 +10,13 @@
 *     * Redistributions in binary form must reproduce the above copyright
 *       notice, this list of conditions and the following disclaimer in the
 *       documentation and/or other materials provided with the distribution.
-*     * The name of Jice may not be used to endorse or promote products
+*     * The name of J.C.Wilk may not be used to endorse or promote products
 *       derived from this software without specific prior written permission.
 *
-* THIS SOFTWARE IS PROVIDED BY Jice ``AS IS'' AND ANY
+* THIS SOFTWARE IS PROVIDED BY J.C.WILK ``AS IS'' AND ANY
 * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
 * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-* DISCLAIMED. IN NO EVENT SHALL Jice BE LIABLE FOR ANY
+* DISCLAIMED. IN NO EVENT SHALL J.C.WILK BE LIABLE FOR ANY
 * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
 * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
 * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
@@ -25,7 +25,11 @@
 * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
-// Mingos' Restrictive Precise Angle Shadowcasting
+/*
+* Mingos' Restrictive Precise Angle Shadowcasting (MRPAS)
+* This file was written by Dominik "Mingos" Marczuk.
+* Original implementation: http://umbrarumregnum.110mb.com/downloads/MRPAS.zip
+*/
 
 #include "libtcod.h"
 #include "libtcod_int.h"
@@ -33,31 +37,31 @@
 inline void TCOD_map_compute_fov_restrictive_shadowcasting_quadrant (map_t *m, int player_x, int player_y, int max_radius, bool light_walls, int maxObstacles, int dx, int dy) {
     //octant: vertical edge
     {
-        int i = player_x; //starting X position
-        int j = player_y; //starting Y position
         int iteration = 1; //iteration of the algo for this octant
         bool done = false;
         int totalObstacles = 0;
         int obstaclesInLastLine = 0;
         double startAngle[maxObstacles], endAngle[maxObstacles];
+		double minAngle = 0.0f;
 
         //do while there are unblocked slopes left and the algo is within the map's boundaries
         //scan progressive lines/columns from the PC outwards
-        int x, y = j+(iteration*dy); //the outer slope's coordinates (first processed line)
+        int x, y = player_y+dy; //the outer slope's coordinates (first processed line)
         if (y < 0 || y >= m->height) done = true;
 		while(!done) {
             done = true;
             //process cells in the line
-            int processedCell = 0;
 			double slopesPerCell = 1.0f/(double)(iteration+1);
-            double slopesPerCell2 = 1.0f/(double)(iteration+2);
-            for (x = i; x >= MAX(0,i-iteration) && x <= MIN(m->width-1,i+iteration); x+=dx) {
+			double halfSlopes = slopesPerCell*0.5f;
+			int processedCell = minAngle / slopesPerCell;
+            int minx = MAX(0,player_x-iteration), maxx = MIN(m->width-1,player_x+iteration);
+            for (x = player_x + (processedCell * dx); x >= minx && x <= maxx; x+=dx) {
                 int c = x + (y * m->width);
                 //calculate slopes per cell
                 bool visible = true;
-                double startSlope = (double)processedCell*slopesPerCell2;
-                double centreSlope = (double)(processedCell+1)*slopesPerCell2;
-                double endSlope = (double)(processedCell+1)*slopesPerCell;
+                double startSlope = (double)processedCell*slopesPerCell;
+                double centreSlope = startSlope+halfSlopes;
+                double endSlope = startSlope+slopesPerCell;
                 if (obstaclesInLastLine > 0 && m->cells[c].fov == 0) {
                     int idx = 0;
                     while(visible && idx < obstaclesInLastLine) {
@@ -69,7 +73,7 @@ inline void TCOD_map_compute_fov_restrictive_shadowcasting_quadrant (map_t *m, i
                             if (startSlope >= startAngle[idx] && endSlope <= endAngle[idx])
                                 visible = false;
                         }
-                        if (visible && (m->cells[c-(m->width*dy)].fov == 0 || !m->cells[c-(m->width*dy)].transparent) && (x < m->width-1 && x > 0 && (m->cells[c-(m->width*dy)-dx].fov == 0 || !m->cells[c-(m->width*dy)-dx].transparent))) visible = false;
+                        if (visible && (m->cells[c-(m->width*dy)].fov == 0 || !m->cells[c-(m->width*dy)].transparent) && (x-dx >= 0 && x-dx < m->width && (m->cells[c-(m->width*dy)-dx].fov == 0 || !m->cells[c-(m->width*dy)-dx].transparent))) visible = false;
                         idx++;
                     }
                 }
@@ -78,8 +82,11 @@ inline void TCOD_map_compute_fov_restrictive_shadowcasting_quadrant (map_t *m, i
                     done = false;
                     //if the cell is opaque, block the adjacent slopes
                     if (!m->cells[c].transparent) {
-                        startAngle[totalObstacles] = startSlope;
-                        endAngle[totalObstacles++] = endSlope;
+                        if (minAngle >= startSlope) minAngle = endSlope;
+                        else {
+                        	startAngle[totalObstacles] = startSlope;
+                        	endAngle[totalObstacles++] = endSlope;
+                        }
                         if (!light_walls) m->cells[c].fov = 0;
                     }
                 }
@@ -88,37 +95,37 @@ inline void TCOD_map_compute_fov_restrictive_shadowcasting_quadrant (map_t *m, i
             if (iteration == max_radius) done = true;
             iteration++;
             obstaclesInLastLine = totalObstacles;
-            y = j + (iteration * dy);
+            y += dy;
             if (y < 0 || y >= m->height) done = true;
         }
     }
     //octant: horizontal edge
     {
-        int i = player_x; //starting X position
-        int j = player_y; //starting Y position
         int iteration = 1; //iteration of the algo for this octant
         bool done = false;
         int totalObstacles = 0;
         int obstaclesInLastLine = 0;
         double startAngle[maxObstacles], endAngle[maxObstacles];
+		double minAngle = 0.0f;
 
         //do while there are unblocked slopes left and the algo is within the map's boundaries
         //scan progressive lines/columns from the PC outwards
-        int x = i+(iteration*dx), y; //the outer slope's coordinates (first processed line)
+        int x = player_x+dx, y; //the outer slope's coordinates (first processed line)
         if (x < 0 || x >= m->width) done = true;
 		while(!done) {
             done = true;
             //process cells in the line
-            int processedCell = 0;
 			double slopesPerCell = 1.0f/(double)(iteration+1);
-            double slopesPerCell2 = 1.0f/(double)(iteration+2);
-            for (y = j; y >= MAX(0,j-iteration) && y <= MIN(m->height-1,j+iteration); y+=dy) {
+			double halfSlopes = slopesPerCell*0.5f;
+			int processedCell = minAngle / slopesPerCell;
+            int miny = MAX(0,player_y-iteration), maxy = MIN(m->height-1,player_y+iteration);
+            for (y = player_y + (processedCell * dy); y >= miny && y <= maxy; y+=dy) {
                 int c = x + (y * m->width);
                 //calculate slopes per cell
                 bool visible = true;
-                double startSlope = (double)processedCell*slopesPerCell2;
-                double centreSlope = (double)(processedCell+1)*slopesPerCell2;
-                double endSlope = (double)(processedCell+1)*slopesPerCell;
+                double startSlope = (double)processedCell*slopesPerCell;
+                double centreSlope = startSlope+halfSlopes;
+                double endSlope = startSlope+slopesPerCell;
                 if (obstaclesInLastLine > 0 && m->cells[c].fov == 0) {
                     int idx = 0;
                     while(visible && idx < obstaclesInLastLine) {
@@ -130,7 +137,7 @@ inline void TCOD_map_compute_fov_restrictive_shadowcasting_quadrant (map_t *m, i
                             if (startSlope >= startAngle[idx] && endSlope <= endAngle[idx])
                                 visible = false;
                         }
-                        if (visible && (m->cells[c-dx].fov == 0 || !m->cells[c-dx].transparent) && (y < m->height-1 && y > 0 && (m->cells[c-(m->width*dy)-dx].fov == 0 || !m->cells[c-(m->width*dy)-dx].transparent))) visible = false;
+                        if (visible && (m->cells[c-dx].fov == 0 || !m->cells[c-dx].transparent) && (y-dy >= 0 && y-dy < m->height && (m->cells[c-(m->width*dy)-dx].fov == 0 || !m->cells[c-(m->width*dy)-dx].transparent))) visible = false;
                         idx++;
                     }
                 }
@@ -139,8 +146,11 @@ inline void TCOD_map_compute_fov_restrictive_shadowcasting_quadrant (map_t *m, i
                     done = false;
                     //if the cell is opaque, block the adjacent slopes
                     if (!m->cells[c].transparent) {
-                        startAngle[totalObstacles] = startSlope;
-                        endAngle[totalObstacles++] = endSlope;
+                        if (minAngle >= startSlope) minAngle = endSlope;
+                        else {
+                        	startAngle[totalObstacles] = startSlope;
+                        	endAngle[totalObstacles++] = endSlope;
+                        }
                         if (!light_walls) m->cells[c].fov = 0;
                     }
                 }
@@ -149,7 +159,7 @@ inline void TCOD_map_compute_fov_restrictive_shadowcasting_quadrant (map_t *m, i
             if (iteration == max_radius) done = true;
             iteration++;
             obstaclesInLastLine = totalObstacles;
-            x = i + (iteration * dx);
+            x += dx;
             if (x < 0 || x >= m->width) done = true;
         }
     }

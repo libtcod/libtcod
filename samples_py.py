a8c0bb02d202b2b69444d46e6bd29a928f28d5b7
Jotaf <jotaf98@hotmail.com>
2010-02-11 07:51:59 +0300

    added fast render demo to python sample.

--- samples_py.py
+++ samples_py.py
@@ -1244,6 +1244,219 @@ def render_name(first, key):
 		ng_names.append("======")
 
 #############################################
+# python fast render sample
+#############################################
+try:  #import NumPy
+	from numpy import *
+	numpy_available = True
+except ImportError:
+	numpy_available = False
+
+use_numpy = numpy_available  #default option
+SCREEN_W = SAMPLE_SCREEN_WIDTH
+SCREEN_H = SAMPLE_SCREEN_HEIGHT
+HALF_W = SCREEN_W / 2
+HALF_H = SCREEN_H / 2
+RES_U = 80  #texture resolution
+RES_V = 80
+TEX_STRETCH = 5  #texture stretching with tunnel depth
+SPEED = 15
+LIGHT_BRIGHTNESS = 3.5  #brightness multiplier for all lights (changes their radius)
+LIGHTS_CHANCE = 0.07  #chance of a light appearing
+MAX_LIGHTS = 6
+MIN_LIGHT_STRENGTH = 0.2
+LIGHT_UPDATE = 0.05  #how much the ambient light changes to reflect current light sources
+AMBIENT_LIGHT = 0.8  #brightness of tunnel texture
+
+#the coordinates of all tiles in the screen, as numpy arrays. example: (4x3 pixels screen)
+#xc = [[1, 2, 3, 4], [1, 2, 3, 4], [1, 2, 3, 4]]
+#yc = [[1, 1, 1, 1], [2, 2, 2, 2], [3, 3, 3, 3]]
+if numpy_available:
+	(xc, yc) = meshgrid(range(SCREEN_W), range(SCREEN_H))
+	#translate coordinates of all pixels to center
+	xc = xc - HALF_W
+	yc = yc - HALF_H
+
+noise2d = libtcod.noise_new(2, 0.5, 2.0)
+if numpy_available:  #the texture starts empty
+	texture = zeros((RES_U, RES_V))
+
+#create lists to work without numpy
+texture2 = [0 for i in range(RES_U * RES_V)]
+brightness2 = [0 for i in range(SCREEN_W * SCREEN_H)]
+R2 = [0 for i in range(SCREEN_W * SCREEN_H)]
+G2 = [0 for i in range(SCREEN_W * SCREEN_H)]
+B2 = [0 for i in range(SCREEN_W * SCREEN_H)]
+
+class Light:
+	def __init__(self, x, y, z, r, g, b, strength):
+		self.x, self.y, self.z = x, y, z  #pos.
+		self.r, self.g, self.b = r, g, b  #color
+		self.strength = strength  #between 0 and 1, defines brightness
+
+def render_py(first, key):
+	global use_numpy, frac_t, abs_t, lights, tex_r, tex_g, tex_b, xc, yc, texture, texture2, brightness2, R2, G2, B2
+	
+	if key.c == ord(' ') and numpy_available:  #toggle renderer
+		use_numpy = not use_numpy
+		first = True
+	if first:  #initialize stuff
+		libtcod.console_clear(sample_console)  #render status message
+		if not numpy_available: text = 'NumPy uninstalled, using default renderer'
+		elif use_numpy: text = 'Renderer: NumPy  \nSpacebar to change'
+		else: text = 'Renderer: default\nSpacebar to change'
+		libtcod.console_set_foreground_color(sample_console,libtcod.white)
+		libtcod.console_print_left(sample_console, 1, SCREEN_H - 3, libtcod.BKGND_NONE, text)
+		
+		frac_t = RES_V - 1  #time is represented in number of pixels of the texture, start later in time to initialize texture
+		abs_t = RES_V - 1
+		lights = []  #lights list, and current color of the tunnel texture
+		tex_r, tex_g, tex_b = 0, 0, 0
+	
+	time_delta = libtcod.sys_get_last_frame_length() * SPEED  #advance time
+	frac_t += time_delta  #increase fractional (always < 1.0) time
+	abs_t += time_delta  #increase absolute elapsed time
+	int_t = int(frac_t)  #integer time units that passed this frame (number of texture pixels to advance)
+	frac_t -= int_t  #keep this < 1.0
+	
+	#change texture color according to presence of lights (basically, sum them
+	#to get ambient light and smoothly change the current color into that)
+	ambient_r = AMBIENT_LIGHT * sum([light.r * light.strength for light in lights])
+	ambient_g = AMBIENT_LIGHT * sum([light.g * light.strength for light in lights])
+	ambient_b = AMBIENT_LIGHT * sum([light.b * light.strength for light in lights])
+	alpha = LIGHT_UPDATE * time_delta
+	tex_r = tex_r * (1 - alpha) + ambient_r * alpha
+	tex_g = tex_g * (1 - alpha) + ambient_g * alpha
+	tex_b = tex_b * (1 - alpha) + ambient_b * alpha
+	
+	if int_t >= 1:  #roll texture (ie, advance in tunnel) according to int_t
+		int_t = mod(int_t, RES_V)  #can't roll more than the texture's size (can happen when time_delta is large)
+		int_abs_t = int(abs_t)  #new pixels are based on absolute elapsed time
+		
+		if use_numpy:
+			texture = roll(texture, -int_t, 1)
+			#replace new stretch of texture with new values
+			for v in range(RES_V - int_t, RES_V):
+				for u in range(0, RES_U):
+					tex_v = (v + int_abs_t) / float(RES_V)
+					texture[u,v] = (libtcod.noise_fbm_simplex(noise2d, [u/float(RES_U), tex_v], 32.0) +
+									libtcod.noise_fbm_simplex(noise2d, [1 - u/float(RES_U), tex_v], 32.0))
+			
+		else:  #"roll" texture, without numpy
+			temp = texture2[0 : RES_U*int_t]
+			texture2 = texture2[RES_U*int_t : ]
+			texture2.extend(temp)
+			
+			#replace new stretch of texture with new values
+			for v in range(RES_V - int_t, RES_V):
+				for u in range(0, RES_U):
+					tex_v = (v + int_abs_t) / float(RES_V)
+					texture2[u + v*RES_U] = (
+						libtcod.noise_fbm_simplex(noise2d, [u/float(RES_U), tex_v], 32.0) +
+						libtcod.noise_fbm_simplex(noise2d, [1 - u/float(RES_U), tex_v], 32.0))
+	if use_numpy:
+		#squared distance from center, clipped to sensible minimum and maximum values
+		sqr_dist = xc**2 + yc**2
+		sqr_dist = sqr_dist.clip(1.0 / RES_V, RES_V**2)
+		
+		#one coordinate into the texture, represents depth in the tunnel
+		v = TEX_STRETCH * float(RES_V) / sqr_dist + frac_t
+		v = v.clip(0, RES_V - 1)
+		
+		#another coordinate, represents rotation around the tunnel
+		u = mod(RES_U * (arctan2(yc, xc) / (2 * pi) + 0.5), RES_U)
+		
+		#retrieve corresponding pixels from texture
+		brightness = texture[u.astype(intp), v.astype(intp)] / 4.0 + 0.5
+		
+		#use the brightness map to compose the final color of the tunnel
+		R = brightness * tex_r
+		G = brightness * tex_g
+		B = brightness * tex_b
+	else:
+		i = 0
+		for y in range(-HALF_H, HALF_H):
+			for x in range(-HALF_W, HALF_W):
+				#squared distance from center, clipped to sensible minimum and maximum values
+				sqr_dist = x**2 + y**2
+				sqr_dist = min(max(sqr_dist, 1.0 / RES_V), RES_V**2)
+				
+				#one coordinate into the texture, represents depth in the tunnel
+				v = TEX_STRETCH * float(RES_V) / sqr_dist + frac_t
+				v = min(v, RES_V - 1)
+				
+				#another coordinate, represents rotation around the tunnel
+				u = mod(RES_U * (arctan2(y, x) / (2 * pi) + 0.5), RES_U)
+				
+				#retrieve corresponding pixels from texture
+				brightness = texture2[int(u) + int(v)*RES_U] / 4.0 + 0.5
+				
+				#use the brightness map to compose the final color of the tunnel
+				R2[i] = brightness * tex_r
+				G2[i] = brightness * tex_g
+				B2[i] = brightness * tex_b
+				i += 1
+	
+	#create new light source
+	if libtcod.random_get_float(0, 0, 1) <= time_delta * LIGHTS_CHANCE and len(lights) < MAX_LIGHTS:
+		x = libtcod.random_get_float(0, -0.5, 0.5)
+		y = libtcod.random_get_float(0, -0.5, 0.5)
+		strength = libtcod.random_get_float(0, MIN_LIGHT_STRENGTH, 1.0)
+		
+		color = libtcod.Color(0, 0, 0)  #create bright colors with random hue
+		hue = libtcod.random_get_float(0, 0, 360)
+		libtcod.color_set_hsv(color, hue, 0.5, strength)
+		lights.append(Light(x, y, TEX_STRETCH, color.r, color.g, color.b, strength))
+	
+	#eliminate lights that are going to be out of view
+	lights = [light for light in lights if light.z - time_delta > 1.0 / RES_V]
+	
+	for light in lights:  #render lights
+		#move light's Z coordinate with time, then project its XYZ coordinates to screen-space
+		light.z -= float(time_delta) / TEX_STRETCH
+		xl = light.x / light.z * SCREEN_H
+		yl = light.y / light.z * SCREEN_H
+		
+		if use_numpy:
+			#calculate brightness of light according to distance from viewer and strength,
+			#then calculate brightness of each pixel with inverse square distance law
+			light_brightness = LIGHT_BRIGHTNESS * light.strength * (1.0 - light.z / TEX_STRETCH)
+			brightness = light_brightness / ((xc - xl)**2 + (yc - yl)**2)
+			
+			#make all pixels shine around this light
+			R += brightness * light.r
+			G += brightness * light.g
+			B += brightness * light.b
+		else:
+			i = 0  #same, without numpy
+			for y in range(-HALF_H, HALF_H):
+				for x in range(-HALF_W, HALF_W):
+					light_brightness = LIGHT_BRIGHTNESS * light.strength * (1.0 - light.z / TEX_STRETCH)
+					brightness = light_brightness / ((x - xl)**2 + (y - yl)**2)
+					
+					R2[i] += brightness * light.r
+					G2[i] += brightness * light.g
+					B2[i] += brightness * light.b
+					i += 1
+	
+	if use_numpy:
+		#truncate values
+		R = R.clip(0, 255)
+		G = G.clip(0, 255)
+		B = B.clip(0, 255)
+	
+		#fill the screen with these background colors
+		libtcod.console_fill_background(sample_console, R, G, B)
+	else:
+		#truncate and convert to integer
+		R2 = [int(min(r, 255)) for r in R2]
+		G2 = [int(min(g, 255)) for g in G2]
+		B2 = [int(min(b, 255)) for b in B2]
+		
+		#fill the screen with these background colors
+		libtcod.console_fill_background(sample_console, R2, G2, B2)
+
+#############################################
 # main loop
 #############################################
 class Sample:
@@ -1260,7 +1473,8 @@ samples = (Sample('  True colors        ', render_colors),
            Sample('  Bsp toolkit        ', render_bsp),
            Sample('  Image toolkit      ', render_image),
            Sample('  Mouse support      ', render_mouse),
-           Sample('  Name generator     ', render_name))
+           Sample('  Name generator     ', render_name),
+           Sample('  Python fast render ', render_py))
 cur_sample = 0
 credits_end = False
 first = True

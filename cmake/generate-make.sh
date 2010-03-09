b3031d486b74f4f6de7ac1bc0a4b00310876d6cd
Chris "donblas" Hamons <chris.hamons@microsoft.com>
2010-03-09 09:07:23 +0300

    Detect MSys/MinGW systems correctly (hopefully).

--- cmake/generate-make.sh
+++ cmake/generate-make.sh
@@ -1,10 +1,19 @@
 #!/bin/sh
+
+if [ "${MSYSTEM}" == "MINGW32" ]; then
+echo "Building MSYS/MinGW makefiles."
+echo "------------------------------"
+BUILD_TYPE="MSYS Makefiles"
+else
+BUILD_TYPE=""
+fi
+
 mkdir -p release 
 cd release
-cmake -DCMAKE_BUILD_TYPE=Release -G "MSYS Makefiles" ../../
+cmake -DCMAKE_BUILD_TYPE=Release -G "${BUILD_TYPE}" ../../
 cd .. 
 
 mkdir -p debug 
 cd debug 
-cmake -DCMAKE_BUILD_TYPE=Debug -G "MSYS Makefiles" ../../
+cmake -DCMAKE_BUILD_TYPE=Debug -G "${BUILD_TYPE}" ../../
 cd .. 

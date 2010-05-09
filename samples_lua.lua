25245541063c9a13f86d605f0f7219556b6e0027
jice <jice.nospam@gmail.com>
2010-05-09 14:04:45 +0400

    added noise lua sample (not working. requires swig fix)

--- samples_lua.lua
+++ samples_lua.lua
@@ -35,7 +35,7 @@ function render_colors(key)
 	-- ==== slighty modify the corner colors ====
 	for c=1,4,1 do
 		-- move each corner color
-		component=rng:getInt(0,2)
+		local component=rng:getInt(0,2)
 		if component == 0 then
 			cols[c].Red = cols[c].Red + 5*dirr[c]
 			if cols[c].Red == 255 then dirr[c]=-1
@@ -55,20 +55,20 @@ function render_colors(key)
 	end
 	-- ==== scan the whole screen, interpolating corner colors ====
 	for x=0,SAMPLE_SCREEN_WIDTH-1,1 do
-		xcoef = x/(SAMPLE_SCREEN_WIDTH-1)
+		local xcoef = x/(SAMPLE_SCREEN_WIDTH-1)
 		-- get the current column top and bottom colors
-		top = tcod.color.Interpolate(cols[TOPLEFT], cols[TOPRIGHT],xcoef)
-		bottom = tcod.color.Interpolate(cols[BOTTOMLEFT], cols[BOTTOMRIGHT],xcoef)
+		local top = tcod.color.Interpolate(cols[TOPLEFT], cols[TOPRIGHT],xcoef)
+		local bottom = tcod.color.Interpolate(cols[BOTTOMLEFT], cols[BOTTOMRIGHT],xcoef)
 		for y=0,SAMPLE_SCREEN_HEIGHT-1,1 do
-			ycoef = y/(SAMPLE_SCREEN_HEIGHT-1)
+			local ycoef = y/(SAMPLE_SCREEN_HEIGHT-1)
 			-- get the current cell color
-			curColor = tcod.color.Interpolate(top,bottom,ycoef)
+			local curColor = tcod.color.Interpolate(top,bottom,ycoef)
 			sampleConsole:setCharBackground(x,y,curColor,tcod.Set)
 		end
 	end
 	-- ==== print the text with a random color ====
 	-- get the background color at the text position
-	textColor=sampleConsole:getCharBackground(SAMPLE_SCREEN_WIDTH/2,5)
+	local textColor=sampleConsole:getCharBackground(SAMPLE_SCREEN_WIDTH/2,5)
 	-- and invert it
 	textColor.Red=255-textColor.Red
 	textColor.Green=255-textColor.Green
@@ -76,9 +76,9 @@ function render_colors(key)
 	-- put random text (for performance tests) 
 	for x=0,SAMPLE_SCREEN_WIDTH-1,1 do
 		for y=0,SAMPLE_SCREEN_HEIGHT-1,1 do
-			col=sampleConsole:getCharBackground(x,y)
+			local col=sampleConsole:getCharBackground(x,y)
 			col=tcod.color.Interpolate(col,tcod.color.black,0.5)
-			c=rng:getInt(97,97+25) -- 97 == 'a'
+			local c=rng:getInt(97,97+25) -- 97 == 'a'
 			sampleConsole:setForegroundColor(col)
 			sampleConsole:putChar(x,y,c,tcod.None)
 		end
@@ -176,7 +176,7 @@ function render_lines(key)
 		-- initialize the colored background
 		for x=0,SAMPLE_SCREEN_WIDTH,1 do
 			for y=0,SAMPLE_SCREEN_HEIGHT,1 do
-				col = tcod.Color(0,0,0)
+				local col = tcod.Color(0,0,0)
 				col.Red = x* 255 / (SAMPLE_SCREEN_WIDTH-1)
 				col.Green = (x+y)* 255 / (SAMPLE_SCREEN_WIDTH-1+SAMPLE_SCREEN_HEIGHT-1)
 				col.Blue = y* 255 / (SAMPLE_SCREEN_HEIGHT-1)
@@ -194,7 +194,7 @@ function render_lines(key)
 	-- render the gradient
 	recty=(SAMPLE_SCREEN_HEIGHT-2)*((1.0+math.cos(tcod.system.getElapsedSeconds()))/2.0)
 	for x=0,SAMPLE_SCREEN_WIDTH,1 do
-		col = tcod.Color(0,0,0)
+		local col = tcod.Color(0,0,0)
 		col.Red=x*255/SAMPLE_SCREEN_WIDTH
 		col.Green=x*255/SAMPLE_SCREEN_WIDTH
 		col.Blue=x*255/SAMPLE_SCREEN_WIDTH
@@ -203,18 +203,18 @@ function render_lines(key)
 		sampleConsole:setCharBackground(x,recty+2,col,bkFlag)
 	end
 	-- calculate the segment ends
-	angle = tcod.system.getElapsedSeconds()*2.0
-	cosAngle=math.cos(angle)
-	sinAngle=math.sin(angle)
-	xo = SAMPLE_SCREEN_WIDTH/2*(1 + cosAngle)
-	yo = SAMPLE_SCREEN_HEIGHT/2 + sinAngle * SAMPLE_SCREEN_WIDTH/2
-	xd = SAMPLE_SCREEN_WIDTH/2*(1 - cosAngle)
-	yd = SAMPLE_SCREEN_HEIGHT/2 - sinAngle * SAMPLE_SCREEN_WIDTH/2
+	local angle = tcod.system.getElapsedSeconds()*2.0
+	local cosAngle=math.cos(angle)
+	local sinAngle=math.sin(angle)
+	local xo = SAMPLE_SCREEN_WIDTH/2*(1 + cosAngle)
+	local yo = SAMPLE_SCREEN_HEIGHT/2 + sinAngle * SAMPLE_SCREEN_WIDTH/2
+	local xd = SAMPLE_SCREEN_WIDTH/2*(1 - cosAngle)
+	local yd = SAMPLE_SCREEN_HEIGHT/2 - sinAngle * SAMPLE_SCREEN_WIDTH/2
 
 	-- render the line
 	tcod.line.init(xo,yo,xd,yd)
-	x=xo
-	y=yo
+	local x=xo
+	local y=yo
 	repeat
 		if x >= 0 and y >= 0 and x < SAMPLE_SCREEN_WIDTH and y < SAMPLE_SCREEN_HEIGHT then
 			sampleConsole:setCharBackground(x,y,tcod.color.lightBlue,bkFlag)
@@ -225,6 +225,142 @@ function render_lines(key)
 	sampleConsole:print(2,2,string.format("%s (ENTER to change)",flagNames[(bkFlag%256)+1]))
 end
 
+-- ***************************
+-- noise sample
+-- ***************************
+-- what noise to render ?
+PERLIN=0
+SIMPLEX=1
+WAVELET=2
+FBM_PERLIN=3
+TURBULENCE_PERLIN=4
+FBM_SIMPLEX=5
+TURBULENCE_SIMPLEX=6
+FBM_WAVELET=7
+TURBULENCE_WAVELET=8
+funcName={
+	"1 : perlin noise       ",
+	"2 : simplex noise      ",
+	"3 : wavelet noise      ",
+	"4 : perlin fbm         ",
+	"5 : perlin turbulence  ",
+	"6 : simplex fbm        ",
+	"7 : simplex turbulence ",
+	"8 : wavelet fbm        ",
+	"9 : wavelet turbulence "
+}
+func=PERLIN
+noise=nil
+noise_dx=0
+noise_dy=0
+octaves=4
+hurst=tcod.NoiseDefaultHurst
+lacunarity=tcod.NoiseDefaultLacunarity
+noise_img=nil
+zoom=3
+function render_noise(key) 
+	if noise == nil then
+		noise = tcod.Noise(2,hurst,lacunarity)
+		img = tcod.Image(SAMPLE_SCREEN_WIDTH*2,SAMPLE_SCREEN_HEIGHT*2)
+	end
+	if first then
+		tcod.system.setFps(30) -- fps limited to 30
+	end
+	
+	-- texture animation
+	noise_dx = noise_dx+0.01
+	noise_dy = noise_dy + 0.01
+	-- render the 2d noise function
+	for y=0,2*SAMPLE_SCREEN_HEIGHT-1,1 do
+		for x=0,2*SAMPLE_SCREEN_WIDTH-1,1 do
+			local f = { zoom*x / (2*SAMPLE_SCREEN_WIDTH) + noise_dx, zoom*y / (2*SAMPLE_SCREEN_HEIGHT) + noise_dy } 
+			local value = 0
+			if func == PERLIN then value = noise:getPerlin(f) 
+			elseif func == SIMPLEX then value = noise:getPerlin(f)  
+			elseif func == WAVELET then value = noise:getWavelet(f)
+			elseif func ==  FBM_PERLIN then value = noise:getFbmPerlin(f,octaves)
+			elseif func ==  TURBULENCE_PERLIN then value = noise:getTurbulencePerlin(f,octaves)
+			elseif func ==  FBM_SIMPLEX then value = noise:getFbmSimplex(f,octaves)
+			elseif func ==  TURBULENCE_SIMPLEX then value = noise:getTurbulenceSimplex(f,octaves)
+			elseif func ==  FBM_WAVELET then value = noise:getFbmWavelet(f,octaves)
+			elseif func ==  TURBULENCE_WAVELET then value = noise:getTurbulenceWavelet(f,octaves)
+			end
+			local c=math.floor((value+1.0)/2.0*255)
+			-- use a bluish color
+			local col = tcod.Color(c/2,c/2,c)
+			img:putPixel(x,y,col)
+		end
+	end
+	-- blit the noise image on the console with subcell resolution
+	img:blit2x(sampleConsole,0,0);
+	-- draw a transparent rectangle
+	sampleConsole:setBackgroundColor(tcod.color.grey)
+	local ypos=10
+	if func > WAVELET then ypos = 13 end
+	sampleConsole:rect(2,2,23,ypos,false,tcod.Multiply)
+	for y=2,1+ypos,1 do
+		for x=2,24,1 do
+			local col = sampleConsole:getFore(x,y)
+			col = col * tcod.color.grey
+			sampleConsole:setFore(x,y,col)
+		end
+	end
+	
+	-- draw the text
+	for curfunc=PERLIN,TURBULENCE_WAVELET,1 do
+		if curfunc == func then
+				sampleConsole:setForegroundColor(tcod.color.white)
+				sampleConsole:setBackgroundColor(tcod.color.lightBlue)
+				sampleConsole:printEx(2,2+curfunc,tcod.Set,tcod.LeftAlignment,funcName[curfunc])
+		else
+				sampleConsole:setForegroundColor(tcod.color.grey)
+				sampleConsole:print(2,2+curfunc,funcName[curfunc])
+		end
+	end
+	-- draw parameters
+	sampleConsole:setForegroundColor(tcod.color.white)
+	sampleConsole:print(2,11,string.format("Y/H : zoom (%2.1f)",zoom))
+	if func > WAVELET then
+		sampleConsole:print(2,12,string.format("E/D : hurst (%2.1f)",hurst))
+		sampleConsole:print(2,13,string.format("R/F : lacunarity (%2.1f)",lacunarity))
+		sampleConsole:print(2,14,string.format("T/G : octaves (%2.1f)",octaves))
+	end
+	-- handle keypress
+	if key.KeyCode == tcod.NoKey then return end
+	if key.Character >= '1' and key.Character <= '9' then
+		-- change the noise function
+		func = key.Character - '1'
+	elseif key.Character == 'E' or key.Character == 'e' then
+		-- increase hurst
+		hurst = hurst + 0.1
+		noise = tcod.Noise(2,hurst,lacunarity)
+	elseif key.Character == 'D' or key.Character == 'd' then
+		-- decrease hurst
+		hurst = hurst - 0.1
+		noise = tcod.Noise(2,hurst,lacunarity)
+	elseif key.Character == 'R' or key.Character == 'r' then
+		-- increase lacunarity
+		lacunarity = lacunarity + 0.5
+		noise = tcod.Noise(2,hurst,lacunarity)
+	elseif key.Character == 'F' or key.Character == 'f' then
+		-- decrease lacunarity
+		lacunarity = lacunarity - 0.5
+		noise = tcod.Noise(2,hurst,lacunarity)
+	elseif key.Character == 'T' or key.Character == 't' then
+		-- increase octaves
+		octaves = octaves + 0.5
+	elseif key.Character == 'G' or key.Character == 'g' then
+		-- decrease octaves
+		octaves = octaves - 0.5
+	elseif key.Character == 'Y' or key.Character == 'y' then
+		-- increase zoom
+		zoom = zoom + 0.2
+	elseif key.Character == 'H' or key.Character == 'h' then
+		-- decrease zoom
+		zoom = zoom - 0.2
+	end
+end
+
 
 creditsEnd=false
 
@@ -232,7 +368,8 @@ creditsEnd=false
 samples = {
 	{ name = "  True colors        ", render = render_colors },
 	{ name = "  Offscreen console  ", render = render_offscreen },
-	{ name = "  Line drawing       ", render = render_lines }
+	{ name = "  Line drawing       ", render = render_lines },
+	{ name = "  Noise              ", render = render_noise } 
 }
 nbSamples = table.getn(samples)
 curSample = 1

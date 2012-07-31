756671dfef43e63513fe189bffbe383ed9ca96cd
jice <unknown>
2012-07-31 19:29:32 +0200

    replace libpng with lodepng

--- src/sys_sdl_img_png.c
+++ src/sys_sdl_img_png.c
@@ -32,26 +32,9 @@
 #else
 #include <SDL/SDL.h>
 #endif
-#include <png.h>
+#include "png/lodepng.h"
 #include "libtcod.h"
 #include "libtcod_int.h"
-
-#define png_infopp_NULL (png_infopp)NULL
-#if defined( TCOD_MACOSX ) || defined( TCOD_HAIKU ) 
-#define int_p_NULL (int*)NULL
-#endif
-
-/* Redirected png IO through SDL's rwops mechanism. */
-static void png_read_data(png_structp png_ptr, png_bytep data, png_size_t length) {
-	SDL_RWops *rwops = png_get_io_ptr(png_ptr);
-	rwops->read(rwops,data,length,1);
-}
-static void png_write_data(png_structp png_ptr, png_bytep data, png_size_t length) {
-	SDL_RWops *rwops = png_get_io_ptr(png_ptr);
-	rwops->write(rwops,data,length,1);
-}
-static void png_flush_data(png_structp png_ptr) {}
-
 
 bool TCOD_sys_check_png(const char *filename) {
 	static uint8 magic_number[]={137, 80, 78, 71, 13, 10, 26, 10};
@@ -59,220 +42,67 @@ bool TCOD_sys_check_png(const char *filename) {
 }
 
 SDL_Surface *TCOD_sys_read_png(const char *filename) {
-	png_uint_32 png_width,png_height,y;
-	int png_bit_depth,png_color_type,png_interlace_type;
-	png_structp png_ptr;
-	png_infop info_ptr;
-#if SDL_VERSION_ATLEAST(2,0,0)
-	SDL_RWops *rwops;
-#else
-	FILE *fp;
-#endif
+	unsigned error;
+	unsigned char* image;
+	unsigned width, height, y, bpp;
+	unsigned char* png;
+	size_t pngsize;
+	LodePNGState state;
 	SDL_Surface *bitmap;
-	png_bytep *row_pointers;
 
-#if SDL_VERSION_ATLEAST(2,0,0)
-	if ((rwops = SDL_RWFromFile(filename, "rb")) == NULL)
-#else
-	if ((fp = fopen(filename, "rb")) == NULL)
-#endif
-		return NULL;
-	/* Create and initialize the png_struct with the desired error handler
-	* functions.  If you want to use the default stderr and longjump method,
-	* you can supply NULL for the last three parameters.  We also supply the
-	* the compiler header file version, so that we know if the application
-	* was compiled with a compatible version of the library.  REQUIRED
-	*/
-	png_ptr = png_create_read_struct(PNG_LIBPNG_VER_STRING,NULL, NULL, NULL);
+	lodepng_state_init(&state);
+	/*optionally customize the state*/
 
-	if (png_ptr == NULL)
-	{
-#if SDL_VERSION_ATLEAST(2,0,0)
-		rwops->close(rwops);
-#else
-		fclose(fp);
-#endif
+	lodepng_load_file(&png, &pngsize, filename);
+	lodepng_inspect(&width,&height,&state, png, pngsize);
+	bpp=lodepng_get_bpp(&state.info_png.color);
+
+	if ( bpp == 24 ) {
+		/* don't convert to 32 bits because libtcod's 24bits renderer is faster */
+		state.info_raw.colortype=LCT_RGB;
+	} else if (  bpp != 24 && bpp != 32 ) { 
+		/* paletted png. convert to 24 bits */
+		state.info_raw.colortype=LCT_RGB;
+		state.info_raw.bitdepth=8;
+		bpp=24;
+	}
+	error = lodepng_decode(&image, &width, &height, &state, png, pngsize);
+	free(png);
+	if(error) {
+		printf("error %u: %s\n", error, lodepng_error_text(error));
+		lodepng_state_cleanup(&state);
 		return NULL;
 	}
-
-	/* Allocate/initialize the memory for image information.  REQUIRED. */
-	info_ptr = png_create_info_struct(png_ptr);
-	if (info_ptr == NULL)
-	{
-#if SDL_VERSION_ATLEAST(2,0,0)
-		rwops->close(rwops);
-#else
-		fclose(fp);
-#endif
-		png_destroy_read_struct(&png_ptr, png_infopp_NULL, png_infopp_NULL);
-		return NULL;
-	}
-
-	/* Set error handling if you are using the setjmp/longjmp method (this is
-	* the normal method of doing things with libpng).  REQUIRED unless you
-	* set up your own error handlers in the png_create_read_struct() earlier.
-	*/
-
-	if (setjmp(png_jmpbuf(png_ptr)))
-	{
-		/* Free all of the memory associated with the png_ptr and info_ptr */
-		png_destroy_read_struct(&png_ptr, &info_ptr, png_infopp_NULL);
-#if SDL_VERSION_ATLEAST(2,0,0)
-		rwops->close(rwops);
-#else
-		fclose(fp);
-#endif
-		/* If we get here, we had a problem reading the file */
-		return NULL;
-	}
-
-#if SDL_VERSION_ATLEAST(2,0,0)
-	png_set_read_fn(png_ptr, (voidp)rwops, (png_rw_ptr)png_read_data);
-#else
-	png_init_io(png_ptr, fp);
-#endif
-
-	/*
-	* If you have enough memory to read in the entire image at once,
-	* and you need to specify only transforms that can be controlled
-	* with one of the PNG_TRANSFORM_* bits (this presently excludes
-	* dithering, filling, setting background, and doing gamma
-	* adjustment), then you can read the entire image (including
-	* pixels) into the info structure with this call:
-	*/
-	/*png_read_png(png_ptr, info_ptr, PNG_TRANSFORM_IDENTITY, png_voidp_NULL); */
-
-	/* get info about the image */
-	png_read_info(png_ptr,info_ptr);
-	png_get_IHDR(png_ptr,info_ptr,&png_width,&png_height,&png_bit_depth,&png_color_type,
-		&png_interlace_type,NULL,NULL);
-
-	/* convert the image to a format suitable with libtcod */
-	png_set_strip_16(png_ptr); /* 16 bits channels => 8 bits channels */
-	png_set_packing(png_ptr); /* 1,2,4 bits depth => 24/32 bits depth */
-	if ( png_color_type == PNG_COLOR_TYPE_GRAY ) png_set_expand(png_ptr); /* grayscale => color */
-	if ( png_color_type == PNG_COLOR_TYPE_GRAY_ALPHA ) png_set_gray_to_rgb(png_ptr);
-
-	/* update the image information */
-	png_read_update_info(png_ptr,info_ptr);
-	png_get_IHDR(png_ptr,info_ptr,&png_width,&png_height,&png_bit_depth,&png_color_type,
-		&png_interlace_type,NULL,NULL);
-
+		
 	/* create the SDL surface */
-	bitmap=TCOD_sys_get_surface(png_width,png_height,(int)png_get_channels(png_ptr, info_ptr)== 4);
-
-	/* get row data */
-	row_pointers=(png_bytep *)malloc(sizeof(png_bytep)*png_height);
-	for (y=0; y<  png_height; y++ ) {
-		row_pointers[y]=(png_bytep)(Uint8 *)(bitmap->pixels) + y * bitmap->pitch;
+	bitmap=TCOD_sys_get_surface(width,height,bpp==32);
+	unsigned char *source=image;
+	unsigned int rowsize=width*bpp/8;
+	for (y=0; y<  height; y++ ) {
+		Uint8 *row_pointer=(Uint8 *)(bitmap->pixels) + y * bitmap->pitch;
+		memcpy(row_pointer,source,rowsize);
+		source+=rowsize;
 	}
 
-	/* read png data directly into the SDL surface */
-	png_read_image(png_ptr,row_pointers);
-
-	/* clean up after the read, and free any memory allocated - REQUIRED */
-	png_destroy_read_struct(&png_ptr, &info_ptr, png_infopp_NULL);
-	free(row_pointers);
-
-	/* close the file */
-#if SDL_VERSION_ATLEAST(2,0,0)
-	rwops->close(rwops);
-#else
-	fclose(fp);
-#endif
+	lodepng_state_cleanup(&state);
+	free(image);	
 	return bitmap;
 }
 
 void TCOD_sys_write_png(const SDL_Surface *surf, const char *filename) {
-	png_structp png_ptr;
-	png_infop info_ptr;
-	png_bytep *row_pointers;
-	int y,x;
-#if SDL_VERSION_ATLEAST(2,0,0)
-	SDL_RWops *rwops = SDL_RWFromFile(filename, "wb");
-	if (!rwops) return;
-#else
-	FILE *fp=fopen(filename,"wb");
-	if (!fp) return;
-#endif
-
-	png_ptr = png_create_write_struct(PNG_LIBPNG_VER_STRING,NULL, NULL, NULL);
-
-	if (png_ptr == NULL)
-	{
-#if SDL_VERSION_ATLEAST(2,0,0)
-		rwops->close(rwops);
-#else
-		fclose(fp);
-#endif
-		return;
-	}
-
-	/* Allocate/initialize the memory for image information.  REQUIRED. */
-	info_ptr = png_create_info_struct(png_ptr);
-	if (info_ptr == NULL)
-	{
-#if SDL_VERSION_ATLEAST(2,0,0)
-		rwops->close(rwops);
-#else
-		fclose(fp);
-#endif
-		png_destroy_write_struct(&png_ptr, png_infopp_NULL);
-		return;
-	}
-
-	if (setjmp(png_jmpbuf(png_ptr)))
-	{
-		/* Free all of the memory associated with the png_ptr and info_ptr */
-		png_destroy_read_struct(&png_ptr, &info_ptr, png_infopp_NULL);
-#if SDL_VERSION_ATLEAST(2,0,0)
-		rwops->close(rwops);
-#else
-		fclose(fp);
-#endif
-		/* If we get here, we had a problem reading the file */
-		return;
-	}
-
-#if SDL_VERSION_ATLEAST(2,0,0)
-	png_set_write_fn(png_ptr, (png_rw_ptr)rwops, (png_rw_ptr)png_write_data, (png_flush_ptr)png_flush_data);
-#else
-	png_init_io(png_ptr, fp);
-#endif
-
-	png_set_IHDR(png_ptr,info_ptr,surf->w, surf->h,
-		8,PNG_COLOR_TYPE_RGB, PNG_INTERLACE_NONE, PNG_COMPRESSION_TYPE_DEFAULT, PNG_FILTER_TYPE_DEFAULT);
-
-	/* get row data */
-	row_pointers=(png_bytep *)malloc(sizeof(png_bytep)*surf->h);
-
+	unsigned char *image, *dest=(unsigned char *)malloc(surf->h*surf->w*3*sizeof(char));
+	unsigned x,y;
+	/* SDL uses 32bits format without alpha layer for screen. convert it to 24 bits */
+	image=dest;
 	for (y=0; y<  surf->h; y++ ) {
-/*		TODO : we should be able to use directly the surface data... */
-/*		row_pointers[y]=(png_bytep)(Uint8 *)(surf->pixels) + y * surf->pitch; */
-		row_pointers[y]=(png_bytep)malloc(sizeof(png_byte)*surf->w*3);
 		for (x=0; x < surf->w; x++ ) {
 			Uint8 *pixel=(Uint8 *)(surf->pixels) + y * surf->pitch + x * surf->format->BytesPerPixel;
-			row_pointers[y][x*3]=*((pixel)+surf->format->Rshift/8);
-			row_pointers[y][x*3+1]=*((pixel)+surf->format->Gshift/8);
-			row_pointers[y][x*3+2]=*((pixel)+surf->format->Bshift/8);
+			*dest++=*((pixel)+surf->format->Rshift/8);
+			*dest++=*((pixel)+surf->format->Gshift/8);
+			*dest++=*((pixel)+surf->format->Bshift/8);
 		}
 	}
-
-	png_set_rows(png_ptr,info_ptr,row_pointers);
-
-	png_write_png(png_ptr,info_ptr,PNG_TRANSFORM_IDENTITY,NULL);
-
-#if SDL_VERSION_ATLEAST(2,0,0)
-	rwops->close(rwops);
-#else
-	fclose(fp);
-#endif
-	/* clean up, and free any memory allocated - REQUIRED */
-	png_destroy_write_struct(&png_ptr, &info_ptr);
-
-	for (y=0; y<  surf->h; y++ ) {
-		free(row_pointers[y]);
-	}
-	free(row_pointers);
+	lodepng_encode24_file(filename,image,surf->w,surf->h);
+	free(image);
 }
 

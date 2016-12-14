0c0ef8e31a31b5654f087d5150cdec29501f6b69
Kyle Stewart <4b796c65@gmail.com>
2016-12-13 18:53:27 -0800

    Disable special Python setup.py functionality.
    The makefiles it used to rely on are no longer available.

--- python/setup.py
+++ python/setup.py
@@ -8,10 +8,6 @@ from distutils.util import get_platform
 from distutils import file_util
 from setuptools import setup, Command
 
-# this flag tells the script if this is a sdist build
-# True if Python is a subdirectory, which means we are above it
-IS_PYTHON_SDIST = os.path.exists('python/')
-
 class build_make(Command):
     description = "run the makefile and include the libraries in the build"
 
@@ -27,7 +23,7 @@ class build_make(Command):
         self.makefile = None
         self.make_data = None # {package: files}
         place_dll_dir = 'libtcodpy'
-        
+
         if 'linux' in sys.platform:
             self.makefile = 'makefiles/makefile-linux'
             self.make_data = {place_dll_dir: ['libtcod.so']}
@@ -40,11 +36,11 @@ class build_make(Command):
         else:
             raise StandardError('No makefile exists for the %s platform' %
                                  sys.platform)
-        
+
     def finalize_options(self):
         self.set_undefined_options('build',
                                    ('build_lib', 'build_lib'),)
-        
+
     def run(self):
         cmd = ['make', '-f', self.makefile,'release']
         if self.ignore_errors:
@@ -56,85 +52,68 @@ class build_make(Command):
             for file in files:
                 self.copy_file(os.path.join('.', file),
                                os.path.join(self.build_lib, directory, file))
-       
+
 class build(orig_build):
-    
+
     def initialize_options(self):
         'add the platform name to the build dir to prevent conflicts'
         orig_build.initialize_options(self)
-        
+
         plat_name = self.plat_name or get_platform()
         self.build_lib = os.path.join(self.build_base, 'lib.%s' % plat_name)
-    
+
     # add build_make as a subcommand
     sub_commands = orig_build.sub_commands + [('build_make', None)]
-    
+
 cmdclass={'build': build, 'build_make': build_make}
-    
+
 try:
     from wheel.bdist_wheel import bdist_wheel as orig_bdist_wheel
-    
+
     class bdist_wheel(orig_bdist_wheel):
         """ctypes libraries are "platform specific" "pure Python" modules.
-        
+
         There's no way to tell bdist_wheel this without overwriting its methods
         """
-        
+
         description = "create a ctypes wheel distribution"
-        
+
         def get_tag(self):
             # modified to add the platform tag to pure libraries
             # no other changes
             impl, abi, plat = orig_bdist_wheel.get_tag(self)
-            
+
             plat_name = self.plat_name
             if plat_name is None:
                 plat_name = get_platform()
             plat_name = plat_name.replace('-', '_').replace('.', '_')
-            
+
             return (impl, abi, plat_name)
-    
+
     cmdclass['bdist_wheel'] = bdist_wheel
 except ImportError:
     pass
 
-try:
-    if not IS_PYTHON_SDIST:
-        # move up and copy important setup files for the duration of the script
-        os.chdir('..')
-        file_util.copy_file('python/setup.cfg', 'setup.cfg')
-        file_util.copy_file('python/MANIFEST.in', 'MANIFEST.in')
-        file_util.copy_file('python/setup.py', 'setup.py')
-
-    setup(
+setup(
     # public name, e.g. > pip install libtcod
     name='libtcod',
-    
+
     # generic first release version
     version = '0.1.0',
-    
+
     # package named to be compatible with the tutorial
     packages=['libtcodpy'],
-    package_dir={'libtcodpy':'python/libtcodpy'},
-    
+
     # use added and modified commands
-    cmdclass=cmdclass,
-    
+    # disabled at this moment
+    #cmdclass=cmdclass,
+
     # important metadata
     url = 'https://bitbucket.org/libtcod/libtcod',
     maintainer = '',
     maintainer_email = '',
-    
+
     # optional metadata for pypi
     description = '',
     license = 'Revised BSD License', # 3-clause BSD license
     )
-finally:
-    if not IS_PYTHON_SDIST:
-        # remove the redundant setup files
-        try:
-            os.remove('setup.cfg')
-            os.remove('MANIFEST.in')
-            os.remove('setup.py')
-        except OSError:
-            pass # ignore missing files

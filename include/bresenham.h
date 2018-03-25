62b0b05d67f51b09ef8c086c9e70a2eb001b3ac6
Kyle Stewart <4b796c65@gmail.com>
2018-03-25 08:39:08 -0700

    Deprecate non-reentrant line functions.
    These functions could be error prone, especially if they're used within the
    library.

--- include/bresenham.h
+++ include/bresenham.h
@@ -35,8 +35,12 @@ extern "C" {
 #endif
 typedef bool (*TCOD_line_listener_t) (int x, int y);
 
-TCODLIB_API void TCOD_line_init(int xFrom, int yFrom, int xTo, int yTo);
-TCODLIB_API bool TCOD_line_step(int *xCur, int *yCur); /* advance one step. returns true if we reach destination */
+TCOD_DEPRECATED TCODLIB_API
+void TCOD_line_init(int xFrom, int yFrom, int xTo, int yTo);
+
+TCOD_DEPRECATED TCODLIB_API
+bool TCOD_line_step(int *xCur, int *yCur); /* advance one step. returns true if we reach destination */
+
 /* atomic callback function. Stops when the callback returns false */
 TCODLIB_API bool TCOD_line(int xFrom, int yFrom, int xTo, int yTo, TCOD_line_listener_t listener);
 

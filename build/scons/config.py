65870e8a114cc7695fc12cd73665b3bdb31da8b3
Kyle Stewart <4b796c65@gmail.com>
2018-08-29 21:29:33 -0700

    Add '/nologo' to MSVC build flags.

--- build/scons/config.py
+++ build/scons/config.py
@@ -7,15 +7,18 @@ DEBUG_GCC = {
     #'CFLAGS': ['-Wstrict-prototypes'],
     'CCFLAGS': ['-Wall', '-g'],
 }
-DEBUG_MSVC = {'CCFLAGS': ['-W3', '-DEBUG']}
+DEBUG_MSVC = {
+    'CCFLAGS': ['/nologo', '-W3', '-DEBUG'],
+    'LINKFLAGS': ['/nologo'],
+}
 
 SIZE_GCC = {
     'CCFLAGS': ['-Wall', '-Os', '-flto'],
     'LINKFLAGS': ['-Os', '-flto'],
 }
 SIZE_MSVC = {
-    'CCFLAGS': ['-W3', '-O1', '-GL', '-GS-'],
-    'LINKFLAGS': ['-LTCG'],
+    'CCFLAGS': ['/nologo', '-W3', '-O1', '-GL', '-GS-'],
+    'LINKFLAGS': ['/nologo', '-LTCG'],
 }
 
 PERFORMANCE_GCC = {
@@ -23,8 +26,8 @@ PERFORMANCE_GCC = {
     'LINKFLAGS': ['-O3', '-flto'],
 }
 PERFORMANCE_MSVC = {
-    'CCFLAGS': ['-W3', '-O2', '-GL', '-GS-'],
-    'LINKFLAGS': ['-LTCG'],
+    'CCFLAGS': ['/nologo', '-W3', '-O2', '-GL', '-GS-'],
+    'LINKFLAGS': ['/nologo', '-LTCG'],
 }
 
 RELEASE_GCC = PERFORMANCE_GCC

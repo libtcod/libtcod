53e7816c0b1525288de7d20adc212e93b84cc134
Kyle Stewart <4b796c65@gmail.com>
2018-09-13 19:02:48 -0700

    Update SDL2 to 2.0.8 on TravisCI.
    Min version on Autotools set to 2.0.5

--- build/.travis/before_install.sh
+++ build/.travis/before_install.sh
@@ -14,6 +14,11 @@ set -e
 if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
     export DISPLAY=:99.0
     sh -e /etc/init.d/xvfb start
+
+    # Update SDL2 to a recent version.
+    wget -O - https://www.libsdl.org/release/SDL2-2.0.8.tar.gz | tar xz
+    (cd SDL2-* && ./configure --prefix=$HOME/.local && make -j 3 install)
+    PATH=~/.local/bin:$PATH
 fi
 
 # Install SCons on MacOS via pip

5b57bff19ebbd97ea06850ecabaeb0b9ccc6e739
Kyle Stewart <4b796c65@gmail.com>
2019-01-10 22:19:03 -0800

    Install pywin32 for SCons.

--- .ci/run-scons.bat
+++ .ci/run-scons.bat
@@ -1,4 +1,4 @@
-C:/Python27/python.exe -m pip install --upgrade pip distribute setuptools wheel
+C:/Python27/python.exe -m pip install --upgrade pip distribute setuptools wheel pywin32
 C:/Python27/python.exe -m pip install "scons==3.0.1"
 cd build/scons
 scons develop_all -s dist %SCONSMODE% %SCONSOPTS% MODE=RELEASE

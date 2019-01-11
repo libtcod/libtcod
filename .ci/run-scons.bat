7a08bd9816900261e4d435f3a51c59f0635913b4
Kyle Stewart <4b796c65@gmail.com>
2019-01-10 20:26:55 -0800

    Lock SCons to version 3.0.1

--- .ci/run-scons.bat
+++ .ci/run-scons.bat
@@ -1,5 +1,5 @@
 C:/Python27/python.exe -m pip install --upgrade pip distribute setuptools wheel
-C:/Python27/python.exe -m pip install scons
+C:/Python27/python.exe -m pip install "scons==3.0.1"
 cd build/scons
 scons develop_all -s dist %SCONSMODE% %SCONSOPTS% MODE=RELEASE
 cd ../..

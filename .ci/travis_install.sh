6be37cc865d566b0e5cb7a45c437503be6441241
Kyle Stewart <4b796c65@gmail.com>
2019-03-08 19:23:20 -0800

    Remove DEBUG_RELEASE from releases.
    Make sure MacOS is always on DEBUG_RELEASE mode.

--- .ci/travis_install.sh
+++ .ci/travis_install.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 if [[ "$BUILD_TOOL" == "scons" ]]; then
     BUILDMODE="MODE=DEBUG_RELEASE"
-    if [[ -n "$TRAVIS_TAG" ]]; then
-        BUILDMODE="MODE=RELEASE"
-    fi
     cd build/scons
     scons develop_all dist -j 3 -s CPPDEFINES=NDEBUG ARCH=x86_64 $BUILDMODE || exit 1
     cd ../..

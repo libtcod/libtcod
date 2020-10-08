3080d3398c021b944b2e4f47651349b3bce922f4
Kyle Stewart <4b796c65+github@gmail.com>
2020-10-08 08:38:50 -0700

    Remove stb_sprintf module.
    This was no longer needed since C99 became the baseline standard for
    libtcod.

--- src/vendor/stb.c
+++ src/vendor/stb.c
@@ -1,8 +1,4 @@
 
-#define STB_SPRINTF_IMPLEMENTATION
-#include "stb_sprintf.h"
-#undef STB_SPRINTF_IMPLEMENTATION
-
 #define STB_TRUETYPE_IMPLEMENTATION
 #include "stb_truetype.h"
 #undef STB_TRUETYPE_IMPLEMENTATION

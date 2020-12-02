64b1ad17ca03727cf4a7b3cce6acf082a9fb4156
Kyle Benesch <4b796c65+github@gmail.com>
2020-12-02 02:20:28 -0800

    Add support for Markdown in the documentation.
    I'm not sure if this will actually be used, in theory it's useful.

--- docs/conf.py
+++ docs/conf.py
@@ -35,6 +35,7 @@ extensions = [
     'sphinx.ext.todo',
     'sphinx.ext.ifconfig',
     'breathe',
+    'recommonmark',
 ]
 
 # Add any paths that contain templates here, relative to this directory.

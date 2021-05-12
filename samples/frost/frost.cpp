c096217348b7975f98ce3631ae45f9e4d353c889
Kyle Benesch <4b796c65+github@gmail.com>
2021-05-12 08:51:54 -0700

    Refactor frost sample.
    Fixed undefined behavior, static member variables like the colors in
    TCODColor can not be used before main.
    
    Replace deprecated libtcod functions with SDL calls.

--- samples/frost/frost.cpp
+++ samples/frost/frost.cpp
@@ -1,12 +1,14 @@
+#include <SDL.h>
 #include <libtcod.h>
-#include <math.h>
-#include <stdio.h>
-#define _SDL_main_h
-#include "libtcod/libtcod_int.h"
 
-TCODColor frostCol[256];
-int keys[4] = {0, 60, 200, 255};
-TCODColor keyCols[4] = {TCODColor::black, TCODColor::darkerBlue, TCODColor::lighterBlue, TCODColor::lightestBlue};
+#include <algorithm>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <memory>
+#include <vector>
+
+std::array<TCODColor, 256> frostCol;
 
 #define GROW 5000.0f
 #define ANGLE_DELAY 0.2f
@@ -20,30 +22,30 @@ struct Frost;
 class FrostManager {
  public:
   FrostManager(int w, int h);
-  ~FrostManager();
   void addFrost(int x, int y);
   void update(float elapsed);
-  void render();
+  void render(TCOD_Console& console);
   void clear();
+  inline bool in_bounds(int cx, int cy) const noexcept { return 0 <= cx && 0 <= cy && cx < w && cy < h; }
   inline float getValue(int cx, int cy) {
-    TCOD_IFNOT(cx + cy * w >= 0 && cx + cy * w < w * h) return 0.0f;
-    return grid[cx + cy * w];
+    if (!(in_bounds(cx, cy))) return 0.0f;
+    return grid.at(cx + cy * w);
   }
   inline void setValue(int cx, int cy, float v) {
-    TCOD_IFNOT(cx + cy * w >= 0 && cx + cy * w < w * h) return;
-    grid[cx + cy * w] = v;
+    if (!(in_bounds(cx, cy))) return;
+    grid.at(cx + cy * w) = v;
   }
 
  protected:
   friend struct Frost;
-  TCODList<Frost*> list;
-  float* grid;
-  TCODImage* img;
+  std::vector<std::unique_ptr<Frost>> list;
+  std::vector<float> grid;
+  std::unique_ptr<TCODImage> img;
   int w, h;
 };
 
 struct Frost {
-  int x, y, bestx, besty, rx, ry;
+  int x, y, best_x, best_y, rx, ry;
   int border;
   FrostManager* manager;
   float timer;
@@ -53,50 +55,33 @@ struct Frost {
   inline float getValue(int cx, int cy) { return manager->getValue(x - RANGE + cx, y - RANGE + cy); }
   inline void setValue(int cx, int cy, float v) { manager->setValue(x - RANGE + cx, y - RANGE + cy, v); }
   bool update(float elapsed);
-  void render(TCODImage* img);
+  void render(TCODImage& img);
 };
 
-FrostManager::FrostManager(int w, int h) : w(w), h(h) {
-  grid = new float[w * h];
-  img = new TCODImage(w, h);
-  clear();
-}
-FrostManager::~FrostManager() {
-  delete[] grid;
-  delete img;
-}
+FrostManager::FrostManager(int w, int h) : grid(w * h), img{std::make_unique<TCODImage>(w, h)}, w{w}, h{h} { clear(); }
+
 void FrostManager::addFrost(int x, int y) {
-  list.push(new Frost(x, y, this));
+  list.emplace_back(std::make_unique<Frost>(x, y, this));
   setValue(x, y, 1.0f);
 }
 
 void FrostManager::clear() {
   img->clear(TCODColor::black);
-  memset(grid, 0, sizeof(float) * w * h);
+  for (auto& it : grid) it = 0;
 }
 
 void FrostManager::update(float elapsed) {
-  TCODList<Frost*> toRemove;
-  for (Frost** it = list.begin(); it != list.end(); it++) {
-    if (!(*it)->update(elapsed)) toRemove.push(*it);
-  }
-  for (Frost** it = toRemove.begin(); it != toRemove.end(); it++) {
-    list.removeFast(*it);
-  }
-  toRemove.clearAndDelete();
+  auto it_end = std::remove_if(list.begin(), list.end(), [&](auto& it) { return !it->update(elapsed); });
+  list.erase(it_end, list.end());
 }
 
-void FrostManager::render() {
-  for (Frost** it = list.begin(); it != list.end(); it++) {
-    (*it)->render(img);
-  }
-  img->blit2x(TCODConsole::root, 0, 0);
+void FrostManager::render(TCOD_Console& console) {
+  for (auto& it : list) it->render(*img);
+  TCOD_image_blit_2x(img->get_data(), &console, 0, 0, 0, 0, -1, -1);
 }
 
-Frost::Frost(int x, int y, FrostManager* manager) : x(x), y(y), manager(manager) {
-  border = 0;
-  timer = 0.0f;
-}
+Frost::Frost(int x, int y, FrostManager* manager) : x{x}, y{y}, manager{manager}, border{0}, timer{0} {}
+
 bool Frost::update(float elapsed) {
   for (int i = PIX_PER_FRAME; i > 0; i--) {
     timer -= elapsed;
@@ -118,8 +103,8 @@ bool Frost::update(float elapsed) {
                 int dist = (cx - rx) * (cx - rx) + (cy - ry) * (cy - ry);
                 if (dist < minDist) {
                   minDist = dist;
-                  bestx = cx;
-                  besty = cy;
+                  best_x = cx;
+                  best_y = cy;
                 }
               }
             }
@@ -134,33 +119,33 @@ bool Frost::update(float elapsed) {
           if (y - RANGE + cy < manager->h - 1 && y - RANGE + cy > 0) {
             if (getValue(cx, cy) < 1.0f) {
               float f = getValue(cx, cy);
-              float oldf = f;
+              float old_f = f;
               f = MAX(f, getValue(cx + 1, cy));
               f = MAX(f, getValue(cx - 1, cy));
               f = MAX(f, getValue(cx, cy + 1));
               f = MAX(f, getValue(cx, cy - 1));
-              setValue(cx, cy, oldf + (f - oldf) * SMOOTH * elapsed);
+              setValue(cx, cy, old_f + (f - old_f) * SMOOTH * elapsed);
             }
           }
         }
       }
     }
-    int curx = bestx;
-    int cury = besty;
+    int cur_x = best_x;
+    int cur_y = best_y;
     // frosting
-    TCODLine::init(curx, cury, rx, ry);
-    TCODLine::step(&curx, &cury);
-    if ((unsigned)(x - RANGE + curx) < (unsigned)manager->w && (unsigned)(y - RANGE + cury) < (unsigned)manager->h) {
-      float f = getValue(curx, cury);
+    TCODLine::init(cur_x, cur_y, rx, ry);
+    TCODLine::step(&cur_x, &cur_y);
+    if ((unsigned)(x - RANGE + cur_x) < (unsigned)manager->w && (unsigned)(y - RANGE + cur_y) < (unsigned)manager->h) {
+      float f = getValue(cur_x, cur_y);
       f += GROW * elapsed;
       f = MIN(1.0f, f);
-      setValue(curx, cury, f);
+      setValue(cur_x, cur_y, f);
       if (f == 1.0f) {
-        bestx = curx;
-        besty = cury;
-        if (bestx == rx && besty == ry) timer = 0.0f;
+        best_x = cur_x;
+        best_y = cur_y;
+        if (best_x == rx && best_y == ry) timer = 0.0f;
         timer = 0.0f;
-        if (curx == 0 || curx == 2 * RANGE || cury == 0 || cury == 2 * RANGE) {
+        if (cur_x == 0 || cur_x == 2 * RANGE || cur_y == 0 || cur_y == 2 * RANGE) {
           border++;
           if (border == 20) return false;
         }
@@ -170,39 +155,68 @@ bool Frost::update(float elapsed) {
   }
   return true;
 }
-void Frost::render(TCODImage* img) {
+void Frost::render(TCODImage& img) {
   int w, h;
-  img->getSize(&w, &h);
-  for (int cx = x - RANGE; cx <= x + RANGE; cx++) {
-    if ((unsigned)cx < (unsigned)w) {
-      for (int cy = y - RANGE; cy <= y + RANGE; cy++) {
-        if ((unsigned)cy < (unsigned)h) {
-          float f = getValue(cx - (x - RANGE), cy - (y - RANGE));
-          int idx = (int)(f * 255);
-          idx = MIN(255, idx);
-          img->putPixel(cx, cy, frostCol[idx]);
-        }
-      }
+  img.getSize(&w, &h);
+  for (int cy = std::max(y - RANGE, 0); cy < std::min(y + RANGE + 1, h); ++cy) {
+    for (int cx = std::max(x - RANGE, 0); cx < std::min(x + RANGE + 1, w); ++cx) {
+      float f = getValue(cx - (x - RANGE), cy - (y - RANGE));
+      int idx = std::max(0, std::min(static_cast<int>(f * 255), 255));
+      img.putPixel(cx, cy, frostCol.at(idx));
     }
   }
 }
 
 int main(int argc, char** argv) {
-  TCODConsole::initRoot(80, 50, "frost test", false, TCOD_RENDERER_OPENGL2);
-  TCOD_mouse_t mouse;
-  TCOD_key_t key;
-  TCODSystem::setFps(25);
-  TCODColor::genMap(frostCol, sizeof(keys) / sizeof(int), keyCols, keys);
+  tcod::ConsolePtr console = tcod::new_console(80, 50);
+  TCOD_ContextParams params = {TCOD_COMPILEDVERSION};
+  params.argc = argc;
+  params.argv = argv;
+  params.columns = console->w;
+  params.rows = console->h;
+  params.window_title = "frost test";
+  params.sdl_window_flags = SDL_WINDOW_RESIZABLE;
+  params.vsync = true;
+  tcod::ContextPtr context = tcod::new_context(params);
+
+  const std::array<int, 4> keys{0, 60, 200, 255};
+  const std::array<TCODColor, 4> keyCols{
+      TCODColor::black, TCODColor::darkerBlue, TCODColor::lighterBlue, TCODColor::lightestBlue};
+  TCODColor::genMap(&frostCol[0], static_cast<int>(keys.size()), keyCols.data(), keys.data());
+
+  uint32_t last_time_ms = SDL_GetTicks();
   FrostManager frostManager(160, 100);
-  while (!TCODConsole::isWindowClosed()) {
-    frostManager.render();
-    TCODConsole::flush();
-    TCODSystem::checkForEvent(TCOD_EVENT_KEY_RELEASE | TCOD_EVENT_MOUSE, &key, &mouse);
-    if (key.vk == TCODK_BACKSPACE) frostManager.clear();
-    if (mouse.lbutton) {
-      frostManager.addFrost(mouse.cx * 2, mouse.cy * 2);
+  while (true) {
+    frostManager.render(*console);
+    context->present(*console);
+
+    SDL_Event event;
+    while (SDL_PollEvent(&event)) {
+      switch (event.type) {
+        case SDL_KEYDOWN:
+          if (event.key.keysym.sym == SDLK_BACKSPACE) frostManager.clear();
+          break;
+        case SDL_MOUSEBUTTONDOWN: {
+          auto tile_xy = context->pixel_to_tile_coordinates(std::array<int, 2>{{event.motion.x, event.motion.y}});
+          if (event.button.button == SDL_BUTTON_LEFT) {
+            frostManager.addFrost(tile_xy.at(0) * 2, tile_xy.at(1) * 2);
+          }
+        }
+        case SDL_MOUSEMOTION: {
+          auto tile_xy = context->pixel_to_tile_coordinates(std::array<int, 2>{{event.motion.x, event.motion.y}});
+          if (event.motion.state & SDL_BUTTON_LMASK) {
+            frostManager.addFrost(tile_xy.at(0) * 2, tile_xy.at(1) * 2);
+          }
+        } break;
+        case SDL_QUIT:
+          std::exit(EXIT_SUCCESS);
+          break;
+      }
     }
-    frostManager.update(TCODSystem::getLastFrameLength());
+    uint32_t current_time_ms = SDL_GetTicks();
+    int delta_time_ms = std::max<int>(0, current_time_ms - last_time_ms);
+    last_time_ms = current_time_ms;
+    frostManager.update(delta_time_ms / 1000.0f);
   }
   return 0;
 }

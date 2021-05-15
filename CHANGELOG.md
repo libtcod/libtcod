125e23dde424bd3432b79e4eee62d7ce6e01f7e4
Kyle Benesch <4b796c65+github@gmail.com>
2021-05-15 15:20:20 -0700

    Prepare 1.17.1 release.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,8 @@ This project attempts to adhere to [Semantic Versioning](http://semver.org/)
 since `1.7.0`.
 
 ## [Unreleased]
+
+## [1.17.1] - 2021-05-15
 ### Fixed
 - Fixed regression with loading PNG images.
 
--- src/libtcod/version.h
+++ src/libtcod/version.h
@@ -34,9 +34,9 @@
 
 #define TCOD_MAJOR_VERSION  1
 #define TCOD_MINOR_VERSION  17
-#define TCOD_PATCHLEVEL     0
+#define TCOD_PATCHLEVEL     1
 
-#define TCOD_STRVERSION "1.17.0"
+#define TCOD_STRVERSION "1.17.1"
 
 /**
     Converts version numbers into a numeric value.

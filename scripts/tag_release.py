53029a1abb65dc56b03980522c9a1a757aaa70a3
Kyle Benesch <4b796c65+github@gmail.com>
2021-05-19 12:04:42 -0700

    Add vcpkg manifest file.
    Refactor tag_release.py script with linters and formatters.

--- scripts/tag_release.py
+++ scripts/tag_release.py
@@ -1,55 +1,62 @@
 #!/usr/bin/env python3
-import sys
-
 import argparse
 import datetime
+import json
 import re
 import subprocess
+import sys
 from typing import Tuple
 
 parser = argparse.ArgumentParser(
-    description='Tags and releases the next version of this project.',
+    description="Tags and releases the next version of this project.",
 )
 
 parser.add_argument(
-    'tag',
-    help='Semantic version number to use as the tag.',
+    "tag",
+    help="Semantic version number to use as the tag.",
 )
 
 parser.add_argument(
-    '-e', '--edit',
-    action='store_true',
+    "-e",
+    "--edit",
+    action="store_true",
     help="Force edits of git commits.",
 )
 
 parser.add_argument(
-    '-n', '--dry-run',
-    action='store_true',
+    "-n",
+    "--dry-run",
+    action="store_true",
     help="Don't modify files.",
 )
 
 parser.add_argument(
-    '-v', '--verbose',
-    action='store_true',
-    help='Print debug information.',
+    "-v",
+    "--verbose",
+    action="store_true",
+    help="Print debug information.",
 )
 
-def parse_changelog(args) -> Tuple[str, str]:
-    """Return an updated changelog and and the list of changes.
-    """
-    with open('CHANGELOG.md', 'r') as file:
+
+def parse_changelog(args: argparse.Namespace) -> Tuple[str, str]:
+    """Return an updated changelog and and the list of changes."""
+    with open("CHANGELOG.md", "r") as file:
         match = re.match(
-            pattern=r'(.*?## \[Unreleased]\n)(.+?\n)(\n*## \[.*)',
+            pattern=r"(.*?## \[Unreleased]\n)(.+?\n)(\n*## \[.*)",
             string=file.read(),
             flags=re.DOTALL,
         )
+    assert match
     header, changes, tail = match.groups()
-    tagged = '\n## [%s] - %s\n%s' % (
-        args.tag, datetime.date.today().isoformat(), changes)
+    tagged = "\n## [%s] - %s\n%s" % (
+        args.tag,
+        datetime.date.today().isoformat(),
+        changes,
+    )
     if args.verbose:
         print(tagged)
 
-    return ''.join((header, tagged, tail)), changes
+    return "".join((header, tagged, tail)), changes
 
 
 VERSION_SOURCE = """\
@@ -61,30 +68,46 @@ VERSION_SOURCE = """\
 #define TCOD_STRVERSION "{tag}"
 {tail}"""
 
-def update_version_header(args):
+
+def update_version_header(args: argparse.Namespace) -> None:
     """Update version.h to use the given tag."""
-    with open('src/libtcod/version.h', 'r') as f:
+    with open("src/libtcod/version.h", "r") as f:
         match = re.match(
-            pattern=r'(?P<head>.*?)\n#define TCOD_MAJOR_VERSION.*?TCOD_STRVERSION[^\n]*\n(?P<tail>.*)',
+            pattern=r"(?P<head>.*?)\n#define TCOD_MAJOR_VERSION.*?TCOD_STRVERSION[^\n]*\n(?P<tail>.*)",
             string=f.read(),
             flags=re.DOTALL,
         )
-    fargs = {'tag': args.tag}
-    fargs.update(match.groupdict())
+    assert match
+    format_args = {"tag": args.tag}
+    format_args.update(match.groupdict())
 
     match = re.match(
-        pattern=r'(?P<major>[0-9]+)\.(?P<minor>[0-9]+)\.(?P<patch>[0-9]+)',
+        pattern=r"(?P<major>[0-9]+)\.(?P<minor>[0-9]+)\.(?P<patch>[0-9]+)",
         string=args.tag,
     )
-    fargs.update(match.groupdict())
+    assert match
+    format_args.update(match.groupdict())
 
-    source = VERSION_SOURCE.format(**fargs)
+    source = VERSION_SOURCE.format(**format_args)
 
     if not args.dry_run:
-        with open('src/libtcod/version.h', 'w') as f:
+        with open("src/libtcod/version.h", "w") as f:
             f.write(source)
 
-def main():
+
+def update_vcpkg_manifest(args: argparse.Namespace) -> None:
+    """Update the version on the vcpkg.json manifest."""
+    with open("vcpkg.json", "r") as f:
+        vcpkg_manifest = json.load(f)
+    vcpkg_manifest["version-semver"] = args.tag
+
+    if not args.dry_run:
+        with open("vcpkg.json", "w") as f:
+            json.dump(vcpkg_manifest, f, indent=2)
+            f.write("\n")
+
+
+def main() -> None:
     if len(sys.argv) == 1:
         parser.print_help(sys.stderr)
         sys.exit(1)
@@ -96,17 +119,20 @@ def main():
     update_version_header(args)
     new_changelog, changes = parse_changelog(args)
 
+    update_vcpkg_manifest(args)
+
     if not args.dry_run:
-        with open('CHANGELOG.md', 'w') as f:
+        with open("CHANGELOG.md", "w") as f:
             f.write(new_changelog)
-        edit = ['-e'] if args.edit else []
+        edit = ["-e"] if args.edit else []
         subprocess.check_call(
-            ['git', 'commit', '-avm', 'Prepare %s release.' % args.tag] + edit,
+            ["git", "commit", "-avm", "Prepare %s release." % args.tag] + edit,
         )
         subprocess.check_call(
-            ['git', 'tag', args.tag, '-a', '-m',
-             '%s\n\n%s' % (args.tag, changes)] + edit,
+            ["git", "tag", args.tag, "-a", "-m", "%s\n\n%s" % (args.tag, changes)]
+            + edit,
         )
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()

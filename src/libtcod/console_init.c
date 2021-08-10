e84de5576c5b13a51ad0851bc23f01c637114a7b
Kyle Benesch <4b796c65+github@gmail.com>
2021-08-10 01:01:22 -0700

    Fix crash when using SDL software renderer.
    This renderer doesn't support VSync and can't be created with it enabled.

--- src/libtcod/console_init.c
+++ src/libtcod/console_init.c
@@ -73,7 +73,8 @@ TCOD_Error TCOD_console_init_root_(
   return TCOD_context_new(&params, &TCOD_ctx.engine);
 }
 TCOD_Error TCOD_console_init_root(int w, int h, const char* title, bool fullscreen, TCOD_renderer_t renderer) {
-  return TCOD_console_init_root_(w, h, title, fullscreen, renderer, true);
+  const bool vsync = renderer != TCOD_RENDERER_SDL;  // Enable VSync for all except the software renderer.
+  return TCOD_console_init_root_(w, h, title, fullscreen, renderer, vsync);
 }
 void TCOD_quit(void) { TCOD_console_delete(NULL); }
 void TCOD_console_set_window_title(const char* title) {

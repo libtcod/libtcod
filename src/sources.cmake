e4bdbdcd308224e03e1cfb40c6c9d566a2040338
Kyle Benesch <4b796c65+github@gmail.com>
2023-08-08 23:50:12 -0700

    Move FlatList inline

--- src/sources.cmake
+++ src/sources.cmake
@@ -154,7 +154,6 @@ target_sources(${PROJECT_NAME} PRIVATE
     libtcod/zip_c.c
     libtcod/gui/button.hpp
     libtcod/gui/container.hpp
-    libtcod/gui/flatlist.cpp
     libtcod/gui/flatlist.hpp
     libtcod/gui/gui.hpp
     libtcod/gui/gui_portability.hpp
@@ -454,7 +453,6 @@ source_group(libtcod FILES
 source_group(libtcod\\gui FILES
     libtcod/gui/button.hpp
     libtcod/gui/container.hpp
-    libtcod/gui/flatlist.cpp
     libtcod/gui/flatlist.hpp
     libtcod/gui/gui.hpp
     libtcod/gui/gui_portability.hpp

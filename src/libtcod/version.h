92ff824c0705d073d5de05a1399539515544256e
Kyle Benesch <4b796c65+github@gmail.com>
2025-04-02 02:36:22 -0700

    Prepare 2.1.1 release.

--- src/libtcod/version.h
+++ src/libtcod/version.h
@@ -35,9 +35,9 @@
 
 #define TCOD_MAJOR_VERSION 2
 #define TCOD_MINOR_VERSION 1
-#define TCOD_PATCHLEVEL 0
+#define TCOD_PATCHLEVEL 1
 
-#define TCOD_STRVERSION "2.1.0"
+#define TCOD_STRVERSION "2.1.1"
 
 /**
     Converts version numbers into a numeric value.

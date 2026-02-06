33c323a5a2a42668918f651c7f58ada7b1b0465e
Kyle Benesch <4b796c65+github@gmail.com>
2026-02-05 20:07:40 -0800

    Move bresenham docs to headers

--- src/libtcod/bresenham.h
+++ src/libtcod/bresenham.h
@@ -54,15 +54,59 @@ extern "C" {
  *          next x,y point on the line.
  */
 typedef bool (*TCOD_line_listener_t)(int x, int y);
-
+/**
+ *  \brief Initialize a line using a global state.
+ *
+ *  \param xFrom The starting x position.
+ *  \param yFrom The starting y position.
+ *  \param xTo The ending x position.
+ *  \param yTo The ending y position.
+ *
+ *  \verbatim embed:rst:leading-asterisk
+ *  .. deprecated:: 1.6.6
+ *    This function is not reentrant and will fail if a new line is started
+ *    before the last is finished processing.
+ *
+ *    Use TCOD_line_init_mt instead.
+ *  \endverbatim
+ */
 TCODLIB_API TCOD_DEPRECATED("This function is not reentrant. Use TCOD_line_init_mt instead.") void TCOD_line_init(
     int xFrom, int yFrom, int xTo, int yTo);
-
-/** advance one step. returns true if we reach destination */
+/**
+ *  \brief Advance to the next point in a line, returns true once the line has ended.
+ *
+ *  \param xCur An int pointer to fill with the next x position.
+ *  \param yCur An int pointer to fill with the next y position.
+ *  \return true once the ending point has been reached.
+ *
+ *  The starting point is excluded by this function.
+ *  After the ending point is reached, the next call will return true.
+ *
+ *  \verbatim embed:rst:leading-asterisk
+ *  .. deprecated:: 1.6.6
+ *    This function is not reentrant and will fail if a new line is started
+ *    before the last is finished processing.
+ *
+ *    Use TCOD_line_step_mt instead.
+ *  \endverbatim
+ */
 TCODLIB_API TCOD_DEPRECATED("This function is not reentrant.") bool TCOD_line_step(
     int* __restrict xCur, int* __restrict yCur);
-
-/* atomic callback function. Stops when the callback returns false */
+/**
+ *  \brief Iterate over a line using a callback.
+ *
+ *  \param xo The origin x position.
+ *  \param yo The origin y position.
+ *  \param xd The destination x position.
+ *  \param yd The destination y position.
+ *  \param listener A TCOD_line_listener_t callback. Iteration stops early if this callback returns false.
+ *  \return true if the line was completely exhausted by the callback.
+ *
+ *  \verbatim embed:rst:leading-asterisk
+ *  .. versionchanged:: 1.6.6
+ *    This function is now reentrant.
+ *  \endverbatim
+ */
 TCODLIB_API bool TCOD_line(int xFrom, int yFrom, int xTo, int yTo, TCOD_line_listener_t listener);
 /**
  *  \brief A struct used for computing a bresenham line.
@@ -78,10 +122,48 @@ typedef struct {
   int destx;
   int desty;
 } TCOD_bresenham_data_t;
-
+/**
+ *  \brief Initialize a TCOD_bresenham_data_t struct.
+ *
+ *  \param xFrom The starting x position.
+ *  \param yFrom The starting y position.
+ *  \param xTo The ending x position.
+ *  \param yTo The ending y position.
+ *  \param data Pointer to a TCOD_bresenham_data_t struct.
+ *
+ *  After calling this function you use TCOD_line_step_mt to iterate
+ *  over the individual points on the line.
+ */
 TCODLIB_API void TCOD_line_init_mt(int xFrom, int yFrom, int xTo, int yTo, TCOD_bresenham_data_t* data);
+/**
+ *  \brief Get the next point on a line, returns true once the line has ended.
+ *
+ *  \param xCur An int pointer to fill with the next x position.
+ *  \param yCur An int pointer to fill with the next y position.
+ *  \param data Pointer to a initialized TCOD_bresenham_data_t struct.
+ *  \return true after the ending point has been reached.
+ *
+ *  The starting point is excluded by this function.
+ *  After the ending point is reached, the next call will return true.
+ */
 TCODLIB_API bool TCOD_line_step_mt(int* __restrict xCur, int* __restrict yCur, TCOD_bresenham_data_t* __restrict data);
-
+/**
+ *  \brief Iterate over a line using a callback.
+ *
+ *  \param xo The origin x position.
+ *  \param yo The origin y position.
+ *  \param xd The destination x position.
+ *  \param yd The destination y position.
+ *  \param listener A TCOD_line_listener_t callback.
+ *  \param data Pointer to a TCOD_bresenham_data_t struct.
+ *  \return true if the line was completely exhausted by the callback.
+ *
+ *  \verbatim embed:rst:leading-asterisk
+ *  .. deprecated:: 1.6.6
+ *    The `data` parameter for this call is redundant, you should call
+ *    TCOD_line instead.
+ *  \endverbatim
+ */
 TCOD_DEPRECATED("Use TCOD_line instead.")
 TCODLIB_API bool TCOD_line_mt(
     int xFrom, int yFrom, int xTo, int yTo, TCOD_line_listener_t listener, TCOD_bresenham_data_t* data);
